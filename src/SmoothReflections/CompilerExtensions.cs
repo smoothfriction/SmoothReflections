48d17e8f18569ac99188edf68334bdba1eda1273
Erik van Brakel <erik@smoothfriction.nl>
2011-02-07 02:08:57 +0100

    First commit

---
+++ src/SmoothReflections/CompilerExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using Microsoft.CSharp;
+
+namespace System.Runtime.CompilerServices
+{
+    public class ExtensionAttribute : Attribute { }
+}
+
+
+namespace SmoothReflections
+{
+    public static class MyExtension
+    {
+        public static string ToText(this CodeExpression expression)
+        {
+            var sb = new StringBuilder();
+            using (var tw = new StringWriter(sb))
+            {
+                var cs = (CSharpCodeProvider)CodeDomProvider.CreateProvider("csharp");
+                cs.GenerateCodeFromExpression(expression, tw, new CodeGeneratorOptions());
+            }
+            return sb.ToString();
+        }
+        public static string ToText(this CodeStatement statement)
+        {
+            var sb = new StringBuilder();
+            using (var tw = new StringWriter(sb))
+            {
+                var cs = (CSharpCodeProvider)CodeDomProvider.CreateProvider("csharp");
+                cs.GenerateCodeFromStatement(statement, tw, new CodeGeneratorOptions());
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/SmoothReflections/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+
+namespace SmoothReflections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = AssemblyDefinition.ReadAssembly(@"C:\Windows\Microsoft.net\Framework\v2.0.50727\mscorlib.dll");
+            var systemObject = assembly.Modules[0].Types.First(x => x.Name == "Object");
+            var ast = MSILtoCSharpConverter.ToAST(systemObject, "GetFieldInfo");
+            var code = MSILtoCSharpConverter.ToCsharp(ast);
+            Console.WriteLine(code);
+            Console.ReadLine();
+        }
+    }
+}
